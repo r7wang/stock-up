b023684b530e3714fe238e1ce08a388bb8a99e7e
Ray Wang <r7wang@gmail.com>
2020-05-02 16:56:24 -0400

    Add common logging framework.

--- stock_common/utils.py
+++ stock_common/utils.py
@@ -1,11 +1,13 @@
 import time
 from typing import Any, Callable, Type
 
+from stock_common.log import logger
+
 
 def retry(func: Callable, num_retries: int, exception_type: Type, error_message: str) -> Any:
     for i in range(num_retries + 1):
         try:
             return func()
         except exception_type:
-            print('{} {}'.format(error_message, i))
+            logger.info('{} {}'.format(error_message, i))
             time.sleep(1)
